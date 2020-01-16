fd17871e4f80acb09584291e166037a9fa72d817
Karel van Klink <karel@deob.us>
2020-01-16 12:14:44 +0100

    misschien werkt dit je weet maar nooit

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ def play_loop():
 
     while True:
         if playing:
-            runstr = 'sox -t raw -c 2 -r 44k -e signed-integer -L -b 16 /opt/music/spotify -t .wav - | pi_fm_rds -freq {0} -pi 6969 -ps {1} -rt "{2}" -audio -'
-            frequency = '107.5'
-            radio_name = 'BAGGAFM'
-            radio_text = "RADIO BAGGA: De Zweethut van de Ziel"
+            # runstr = 'sox -t raw -c 2 -r 44k -e signed-integer -L -b 16 /opt/music/spotify -t .wav - | pi_fm_rds -freq {0} -pi 6969 -ps {1} -rt "{2}" -audio -'
+            # frequency = '107.5'
+            # radio_name = 'BAGGAFM'
+            # radio_text = "RADIO BAGGA: De Zweethut van de Ziel"
 
-            p = subprocess.Popen(runstr.format(frequency, radio_name, radio_text), preexec_fn=os.setsid)
+            # p = subprocess.Popen(runstr.format(frequency, radio_name, radio_text), preexec_fn=os.setsid)
+
+            p = subprocess.Popen("huts", preexec_fn=os.setsid)
             p.communicate()
 
 
