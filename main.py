e6aaeaf6114f90b14c0d731c4d7d19a1d8052b4e
Karel van Klink <karel@deob.us>
2020-01-16 15:51:48 +0100

    add /update endpoint

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import signal
 import subprocess
 import threading
 
-from flask import Flask, jsonify
+import requests
+from flask import Flask, jsonify, request
 from tendo import singleton
 
 app = Flask(__name__, static_url_path='')
 
 playing = False
 p = None
+radio_text = "RADIO BAGGA: De Zweethut van de Ziel"
 
 
 @app.before_first_request
@@ -20,18 +22,19 @@ def startup():
 
 def play_loop():
     global p
+    global playing
+    global radio_text
     me = singleton.SingleInstance()
 
     while True:
         if playing:
-            # runstr = 'sox -t raw -c 2 -r 44k -e signed-integer -L -b 16 /opt/music/spotify -t .wav - | pi_fm_rds -freq {0} -pi 6969 -ps {1} -rt "{2}" -audio -'
-            # frequency = '107.5'
-            # radio_name = 'BAGGAFM'
-            # radio_text = "RADIO BAGGA: De Zweethut van de Ziel"
+            runstr = 'sox -t raw -c 2 -r 44k -e signed-integer -L -b 16 /opt/music/spotify -t .wav - | ' \
+                     'pi_fm_rds -freq {0} -pi 6969 -ps {1} -rt "{2}" -audio -'
+            frequency = '107.5'
+            radio_name = 'BAGGAFM'
 
-            # p = subprocess.Popen(runstr.format(frequency, radio_name, radio_text), preexec_fn=os.setsid)
+            p = subprocess.Popen(runstr.format(frequency, radio_name, radio_text), preexec_fn=os.setsid)
 
-            p = subprocess.Popen(['sudo', 'huts'], preexec_fn=os.setsid)
             p.communicate()
 
 
@@ -48,7 +51,23 @@ def stop():
     return jsonify(success=True)
 
 
+@app.route('/update')
+def update():
+    global radio_text
+
+    track_id = request.args.get('id', default='0')
+    if track_id != '0':
+        r = requests.get("https://api.spotify.com/v1/tracks/" + track_id + "?market=NL",
+                         {'Accept': 'application/json', 'Content-Type': 'application/json',
+                          'Authorization': 'Bearer ' + os.environ['SPOTIFY_API_TOKEN']})
+        data = r.json()
+        radio_text = data['artist'] + ' - ' + data['name']
+        print(radio_text)
+
+
 def shutdown():
+    global playing
+    playing = False
     os.killpg(os.getpgid(p.pid), signal.SIGTERM)
 
 
